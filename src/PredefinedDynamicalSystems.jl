c793e5e5e3f8c0b7d984ac0ad13aa33d7b7e7c40
Datseris <datseris.george@gmail.com>
2023-02-12 14:12:14 +0000

    update to proper package

--- src/PredefinedDynamicalSystems.jl
+++ src/PredefinedDynamicalSystems.jl
@@ -1,64 +1,30 @@
-module DynamicalSystemsBase
+"""
+Module which contains pre-defined dynamical systems used in DynamicalSystems.jl
+The alias `Systems` is exported by `DynamicalSystems`.
 
-# Use the README as the module docs
-@doc let
-    path = joinpath(dirname(@__DIR__), "README.md")
-    include_dependency(path)
-    read(path, String)
-end DynamicalSystemsBase
+This module is provided purely as a convenience.
+Nothing here is tested, nor guaranteed to be stable in future versions.
+
+Predefined systems exist functions that
+return a `DynamicalSystem` instance. They are accessed like:
+```julia
+ds = PredefinedDynamicalSystems.lorenz(u0; ρ = 32.0)
+```
+
+For some systems, a Jacobian function is also defined.
+The naming convention for the jacobian function is `\$(name)_jacob`.
+So, for the above example we have `J = Systems.lorenz_jacob`.
+"""
+module PredefinedDynamicalSystems
 
 using Reexport
-@reexport using StateSpaceSets
-export Systems
+@reexport using DynamicalSystemsBase
+using StaticArrays
 
-import SciMLBase
-import SciMLBase: reinit!, step!, isinplace
-using SciMLBase: recursivecopy
-export reinit!, step!, isinplace, recursivecopy
+const DDS = DeterministicIteratedMap
+include("discrete_famous_systems.jl")
 
-include("core/dynamicalsystem_interface.jl")
-include("core/trajectory.jl")
-include("core/utilities.jl")
-include("core/pretty_printing.jl")
-
-include("core_systems/discrete_time_map.jl")
-include("core_systems/continuous_time_ode.jl")
-include("core_systems/arbitrary_steppable.jl")
-include("core_systems/additional_supertypes.jl")
-
-include("derived_systems/stroboscopic_map.jl")
-include("derived_systems/parallel_systems.jl")
-include("derived_systems/tangent_space.jl")
-include("derived_systems/poincare/poincaremap.jl")
-include("derived_systems/projected_system.jl")
-
-include("predefined/famous_systems.jl")
-
-include("deprecations.jl")
-
-# include("core/dynamicalsystem.jl")
-# include("core/create_dynamics.jl")
-# include("core/api_docstrings.jl")
-# include("core/discrete.jl")
-# include("core/continuous.jl")
-# include("core/pretty_printing.jl")
-
-# include("advanced_integrators/projected_integrator.jl")
-# include("advanced_integrators/stroboscopic_map.jl")
-# include("advanced_integrators/poincare.jl")
-
-# include("predefined/famous_systems.jl") # includes both discrete and continuous
-
-# export GeneralizedDynamicalSystem, DynamicalSystem
-# export ContinuousDynamicalSystem, DiscreteDynamicalSystem
-# export rulestring, isdiscretetime
-# export dimension, get_state, get_states
-# export get_parameter, get_parameters, set_parameter!
-# export trajectory, jacobian
-# export integrator, tangent_integrator, parallel_integrator
-# export set_state!, get_state, get_deviations, set_deviations!, current_time
-# export SciMLBase, init, step!, isinplace, reinit!
-# export Systems
-# export projected_integrator, stroboscopicmap
+const CDS = CoupledODEs
+include("continuous_famous_systems.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,4 @@
-using DynamicalSystemsBase
+using PredefinedDynamicalSystems
 using Test
 
-defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
-testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
-
-@testset "DynamicalSystemsBase" begin
-    testfile("discrete.jl")
-    testfile("continuous.jl")
-    testfile("stroboscopic.jl")
-    testfile("parallel.jl")
-    testfile("tangent.jl")
-    testfile("poincare.jl")
-    testfile("projected.jl")
-end
+@test PredefinedDynamicalSystems.henon() isa DeterministicIteratedMap
