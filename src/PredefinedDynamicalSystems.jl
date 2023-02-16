cd62bf2da8dce9e5aecfcdac75d4eaa78f30affe
George Datseris <datseris.george@gmail.com>
2023-02-16 15:30:07 +0000

    Finish and tag v1 (#2)
    * remove old files
    
    * export Syxtems
    
    * make compilable
    
    * add docs
    
    * better read me and module
    
    * buildable docs
    
    * tests pass as far as I can tell
    
    * ad cairomakie to the docbuild

--- src/PredefinedDynamicalSystems.jl
+++ src/PredefinedDynamicalSystems.jl
@@ -1,26 +1,17 @@
-"""
-Module which contains pre-defined dynamical systems used in DynamicalSystems.jl
-The alias `Systems` is exported by `DynamicalSystems`.
-
-This module is provided purely as a convenience.
-Nothing here is tested, nor guaranteed to be stable in future versions.
-It is not recommended to use this module for anything else besides
-on-the-spot demonstrative examples.
-
-Predefined systems exist as functions that
-return a `DynamicalSystem` instance. They are accessed like:
-```julia
-ds = PredefinedDynamicalSystems.lorenz(u0; ρ = 32.0)
-```
-
-For some systems, a Jacobian function is also defined.
-The naming convention for the jacobian function is `\$(name)_jacob`.
-So, for the above example we have `J = Systems.lorenz_jacob`.
-"""
 module PredefinedDynamicalSystems
+# Use the README as the module docs
+@doc let
+    path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    read(path, String)
+end PredefinedDynamicalSystems
+
+const Systems = PredefinedDynamicalSystems
+export Systems
 
 using Reexport
 @reexport using DynamicalSystemsBase
+
 using StaticArrays
 
 const DDS = DeterministicIteratedMap
