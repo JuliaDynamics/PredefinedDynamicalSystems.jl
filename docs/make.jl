3373e25df12cc335b16e71bfb2e0fd5400d8212d
Datseris <datseris.george@gmail.com>
2023-02-25 19:54:40 +0000

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,34 +1,13 @@
 cd(@__DIR__)
+using PredefinedDynamicalSystems
+
+pages = ["index.md"]
 
 import Downloads
 Downloads.download(
-    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/apply_style.jl",
-    joinpath(@__DIR__, "apply_style.jl")
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
 )
-include("apply_style.jl")
+include("build_docs_with_style.jl")
 
-using PredefinedDynamicalSystems
-
-makedocs(
-    modules = [PredefinedDynamicalSystems],
-    format = Documenter.HTML(
-        prettyurls = CI,
-        assets = [
-            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css),
-        ],
-        collapselevel = 3,
-    ),
-    sitename = "PredefinedDynamicalSystems.jl",
-    authors = "George Datseris",
-    pages = ["index.md"],
-    doctest = false,
-    draft = false,
-)
-
-if CI
-    deploydocs(
-        repo = "github.com/JuliaDynamics/PredefinedDynamicalSystems.jl.git",
-        target = "build",
-        push_preview = true
-    )
-end
+build_docs_with_style(pages, PredefinedDynamicalSystems)
